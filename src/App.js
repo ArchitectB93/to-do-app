35532a1f4d1a8112501b17a22a613930d89d8418
ArchitectB93 <107737895+ArchitectB93@users.noreply.github.com>
2023-01-24 17:00:55 -0800

    asdf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="todo-app">
+    <TodoList />
       
     </div>
   );
